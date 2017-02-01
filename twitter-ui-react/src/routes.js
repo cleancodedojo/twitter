4f0c610adc66735db3e50e38117c62421da14a41
raydecastro <captray108@gmail.com>
2017-02-01 03:37:16 -0800

    feature. implemented preliminary user profile page this uses the twitter core module, the UI currently doesn't have any tests.

--- twitter-ui-react/src/routes.js
+++ twitter-ui-react/src/routes.js
@@ -6,10 +6,12 @@ import HomePage from './components/HomePage';
 import FuelSavingsPage from './containers/FuelSavingsPage'; // eslint-disable-line import/no-named-as-default
 import AboutPage from './components/AboutPage';
 import NotFoundPage from './components/NotFoundPage';
+import ProfilePage from './components/ProfilePage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}/>
+    <Route path="profile" component={ProfilePage}/>
     <Route path="fuel-savings" component={FuelSavingsPage}/>
     <Route path="about" component={AboutPage}/>
     <Route path="*" component={NotFoundPage}/>
